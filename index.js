eb2d461777e5d8f153f4826a5fc04d99e9806482
hovapontus <106970440+hovapontus@users.noreply.github.com>
2024-08-07 15:24:06 +0200

    fawf

---
+++ index.js
@@ -0,0 +1,10 @@
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts());
+
+console.log(`Post Length: ${getPostsLength()}`);
+
+
+/* const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
+console.log(`${generateRandomNumber()}`);
+console.log(`Celsius to farenheit: ${celsiusToFahrenheit(0)}`); */
